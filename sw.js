1eddabcdfaec391a29e03ba7399894f74b3c60b8
paulsgh24 <paulskolaude@googlemail.com>
2025-12-02 17:55:12 +0100

    5

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = "v1";
+
+self.addEventListener("install", e => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => key !== CACHE && caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Netzwerk zuerst → wenn offline → Cache
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
